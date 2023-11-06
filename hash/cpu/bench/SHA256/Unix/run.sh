6ebadf41c9c297c26fb40954098e3be7427afc58
Balazs Komuves <bkomuves@gmail.com>
2023-11-06 15:33:09 +0100

    add `bench.cfg` files

--- hash/cpu/bench/SHA256/Unix/run.sh
+++ hash/cpu/bench/SHA256/Unix/run.sh
@@ -6,4 +6,21 @@ fi
 
 echo "MEGABYTES = $ZKBENCH_MEGABYTES"
 
-./build/fakedata $ZKBENCH_MEGABYTES | shasum -a256 -b -
+OSTYPE=`uname -s`
+
+case $OSTYPE in
+  Darwin)
+    ./build/fakedata $ZKBENCH_MEGABYTES | shasum -a256 -b -
+    ;;
+  Linux)  
+    ./build/fakedata $ZKBENCH_MEGABYTES | sha256sum -b -
+    ;;
+  FreeBSD)  
+    ./build/fakedata $ZKBENCH_MEGABYTES | sha256sum -b -
+    ;;
+
+  *)
+    echo "unknown operating system \`$OSTYPE\`"
+    exit 99
+    ;;
+esac
--- hash/snark/bench/Poseidon2/build.sh
+++ hash/snark/bench/Poseidon2/build.sh
@@ -14,8 +14,12 @@ mkdir -p build
 
 gcc -O3 generate_input.c -o build/generate_input || { echo "gcc failed"; exit 101; }
 
-NAME=${ZKBENCH_WHICH}
-sed "s/ZKBENCH_INPUT_SIZE/${ZKBENCH_INPUT_SIZE}/g" ${NAME}.circom.template >build/${NAME}.circom
+#NAME=${ZKBENCH_WHICH}
+NAME="hash"
+echo ${NAME}.circom.template \
+  | sed "s/ZKBENCH_INPUT_SIZE/${ZKBENCH_INPUT_SIZE}/g" \
+  | sed "s/ZKBENCH_WHICH/${ZKBENCH_WHICH}/g"           \
+  >build/${NAME}.circom
 
 cd build
 
