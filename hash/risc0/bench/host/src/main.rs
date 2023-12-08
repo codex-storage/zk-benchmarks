ffb423610a0b9fbe72582c5b8310b0c1f1fda5f7
Manish Kumar <kumarmanish19963@gmail.com>
2023-12-08 12:52:19 +0530

    criterion benchmarking removed, benchmarking using std::time

--- hash/risc0/bench/host/src/main.rs
+++ hash/risc0/bench/host/src/main.rs
@@ -1,4 +1,47 @@
+use methods::{
+    METHOD_ELF, METHOD_ID
+};
+use risc0_zkvm::{default_prover, ExecutorEnv};
+use risc0_zkvm::{ sha};
+// use rand::RngCore;
+use rand::Rng;
+use std::time::Instant;
+
+fn generate_bytes(size: usize) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    (0..size).map(|_| rng.gen()).collect()
+}
+
+pub fn sha_bench(input: Vec<u8>) {
+   
+    let start_time = Instant::now();
+
+    let env = ExecutorEnv::builder().write(&input).unwrap().build().unwrap();
+
+    // Obtain the default prover.
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    let receipt = prover.prove_elf(env, METHOD_ELF).unwrap();
+
+    // TODO: Implement code for retrieving receipt journal here.
+
+    // For example:
+    let _output: sha::Digest = receipt.journal.decode().unwrap();
+   
+    // verify your receipt
+    receipt.verify(METHOD_ID).unwrap();
+
+    let elapsed_time = start_time.elapsed();
+    eprintln!("Total time: {:?}", elapsed_time);
+}
 
 fn main() {
-
+    let args: Vec<String> = std::env::args().collect();
+    eprintln!("{:?}", &args[1]);
+    // eprintln!("{:?}", &args[2]);
+    let size_kb = args[1].parse::<usize>().unwrap();
+    eprintln!("{:?}", size_kb);
+    let input = generate_bytes(size_kb);
+    sha_bench(input);
 }
--- hash/risc0/bench/run.sh
+++ hash/risc0/bench/run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Check if ZKBENCH_INPUT_SIZE_KB is set, otherwise set a default value
-ZKBENCH_INPUT_SIZE_KB=${ZKBENCH_INPUT_SIZE_KB:-"1"}  # Default to 1 if not set
+# Set a default value if ZKBENCH_INPUT_SIZE_KB is not set
+: ${ZKBENCH_INPUT_SIZE_KB:=1024}
 
-# Run benchmarks with the specified input size
-if [ "$ZKBENCH_INPUT_SIZE_KB" = "1" ]; then
-    cargo bench --bench bench_main -- 1
-elif [ "$ZKBENCH_INPUT_SIZE_KB" = "2" ]; then
-    cargo bench --bench bench_main -- 2
-elif [ "$ZKBENCH_INPUT_SIZE_KB" = "10" ]; then
-    cargo bench --bench bench_main -- 10
-else
-    echo "Invalid input size: $ZKBENCH_INPUT_SIZE_KB"
-fi
+# Run cargo run with the specified environment variable
+cargo run $ZKBENCH_INPUT_SIZE_KB
