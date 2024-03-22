d4085d4d0a480ddf0a93e3eefa2babe8333af829
Manish Kumar <kumarmanish19963@gmail.com>
2024-03-22 11:25:17 +0530

    cycle prinitng removed in guest because it increases the overall cycle count

--- hash/risc0/bench/methods/guest/src/bin/poseidon2_babybear.rs
+++ hash/risc0/bench/methods/guest/src/bin/poseidon2_babybear.rs
@@ -7,7 +7,7 @@ use zkhash::poseidon2::poseidon2_instance_babybear::{/*POSEIDON2_BABYBEAR_16_PAR
 use zkhash::fields::babybear::FpBabyBear;
 use ark_serialize::{CanonicalSerialize, CanonicalDeserialize};
 use std::marker::PhantomData;
-use risc0_zkvm::guest::env::cycle_count;
+
 
 use ark_ff::PrimeField;
 
@@ -100,21 +100,17 @@ pub fn main() {
 
     let data: Vec<Vec<u8>> = env::read();
     
-    let cycles1 = cycle_count();
     let mut hash_data: Vec<FpBabyBear> = Vec::new();
     for i in 0..data.len() {
         let a_uncompressed = FpBabyBear::deserialize_uncompressed(&**data.get(i).unwrap()).unwrap();
         hash_data.push(a_uncompressed);
     }
-    let cycles2 = cycle_count();
     
-
     let permutation = poseidon2::Poseidon2::new(&POSEIDON2_BABYBEAR_24_PARAMS);
     let mut merkle_tree = MerkleTree::new(permutation.clone());
-    let cycles3 = cycle_count();
+
     let hash_final = merkle_tree.accumulate(&hash_data);
 
-    let cycles4 = cycle_count();
     
     let mut perm_seralised: Vec<Vec<u8>> = Vec::new();
     for i in 0..8 {
@@ -122,14 +118,8 @@ pub fn main() {
         hash_final.get(i).unwrap().serialize_uncompressed(&mut temp).unwrap();
         perm_seralised.push(temp);
     }
-    let cycles6 = cycle_count();
+
 
     env::commit(&perm_seralised);
-
-    eprintln!("number of cycles for input builder: {:?}", cycles2 - cycles1);
-    eprintln!("number of cycles for hash permutation builder: {:?}", cycles3 - cycles2);
-    eprintln!("number of cycles for hash  accumulation: {:?}", cycles4 - cycles3);
-
-    eprintln!("number of cycles for permutation seralisation: {:?}", cycles6 - cycles4);
 
 }
--- hash/risc0/bench/methods/guest/src/bin/sha256_accelerated.rs
+++ hash/risc0/bench/methods/guest/src/bin/sha256_accelerated.rs
@@ -4,8 +4,7 @@
 use std::io::Read;
 use risc0_zkvm::guest::env;
 use sha2::{Sha256, Digest};
-use risc0_zkvm::guest::env::cycle_count;
-// use base16ct::lower::encode_str;
+
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
@@ -14,10 +13,8 @@ pub fn main() {
     env::stdin().read_to_end(&mut data).unwrap();
 
     let result = Sha256::digest(data);
-    let c1 = cycle_count();
-    eprintln!("total cycle count for hashing: {:?}",c1);
+
     let result_bytes: [u8;32] = result.into();
-    let c2 = cycle_count();
-    eprintln!("cycle count to convert into bytes array: {:?}",c2-c1);
+
     env::commit(&result_bytes)
 }
